cf7a4849b2f3ba5813b09b6bad7ee20e85f52bb5
Jiliber11 <163329312+Jiliber11@users.noreply.github.com>
2024-04-24 08:55:42 -0500

    Add files via upload

---
+++ src/Libro/Libro.java
@@ -0,0 +1,53 @@
+
+
+    
+    package Libro;
+
+public class Libro {
+    // Atributos
+    private String titulo;
+    private String autor;
+    private int ejemplares;
+    private int prestados;
+
+    // Constructores
+    public Libro(String titulo, String autor, int ejemplares, int prestados) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ejemplares = ejemplares;
+        this.prestados = prestados;
+    }
+
+    public Libro() {
+    }
+
+    // Métodos Getter
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getEjemplares() {
+        return ejemplares;
+    }
+
+    public int getPrestados() {
+        return prestados;
+    }
+    
+
+    // Método prestar
+    public boolean prestar() {
+        if (prestados < ejemplares) {
+            prestados++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+    
+
---
+++ src/Libro/PrincipalLibro.java
@@ -0,0 +1,20 @@
+
+package Libro;
+
+public class PrincipalLibro {
+     
+    public static void main(String[] args) {
+                                //(String titulo, String autor, int ejemplares, int prestados)
+        Libro libro = new Libro("Cien años de soledad", "Gabriel García Márquez", 10, 10);
+        System.out.println("Número de ejemplares prestados: " + libro.getPrestados());
+        if (libro.prestar()) {
+            System.out.println("Préstamo exitoso.");
+        } else {
+            System.out.println("No se puede prestar más ejemplares.");
+        }
+    }
+}
+    
+    
+    
+
